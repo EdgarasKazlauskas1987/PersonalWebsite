31ccd51f0f252bb0f5fccb24493d1d110669bad4
edgaras <edgarasvilija@gmail.com>
2017-07-13 16:54:44 +0200

    July 13th commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var nodemailer = require('nodemailer');
 var bodyParser = require('body-parser');
+var projectsDataFile = require('./data/projects-data.json');
 
 var app = express();
 
@@ -10,7 +11,10 @@ app.use(express.static(__dirname + '/'));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+//var projectsDataFile = require("projects-data.json");
+
 app.set('view engine', 'ejs');
+app.set('projectsData', projectsDataFile);
 
 //creating transporter object
 var transporter = nodemailer.createTransport({
@@ -28,7 +32,25 @@ app.get('/', (req, res) =>
 
 app.get('/projects', (req, res) =>
 {
-    res.render('projects');
+  var data = req.app.get('projectsData');
+  var titles = [];
+  var descriptions = [];
+  var languages = [];
+  var gitHubLinks = [];
+
+  data.projects.forEach(function(item) {
+    titles = titles.concat(item.title);
+    descriptions = descriptions.concat(item.description);
+    languages = languages.concat(item.language);
+    gitHubLinks = gitHubLinks.concat(item.gitHub);
+  });
+
+    res.render('projects', {
+      projectTitle: titles,
+      projectDescription: descriptions,
+      projectLanguage: languages,
+      projectGitHubLinks: gitHubLinks
+    });
 });
 
 app.post('/sendEmail', (req, res, next) =>
