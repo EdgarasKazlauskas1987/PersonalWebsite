3da01a201fcea0d91e1d583572a32e1a8e705be4
edgaras <edgarasvilija@gmail.com>
2017-07-28 18:36:10 +0200

    28 of July commit

--- javascript.js
+++ javascript.js
@@ -2,3 +2,18 @@ function resetInquiryForm()
 {
     document.getElementById("inquiry_form").reset();
 }
+
+
+function hideRow(row1, hideRow, showRow ) {
+    document.getElementById(row1).style.display = "none";
+    document.getElementById(hideRow).style.display = "none";
+    document.getElementById(showRow).style.display = "";
+}
+
+function showRow(row1, hideRow, showRow) {
+    document.getElementById(row1).style.display = "";
+    document.getElementById(hideRow).style.display = "";
+    document.getElementById(showRow).style.display = "none";
+   
+}
+
