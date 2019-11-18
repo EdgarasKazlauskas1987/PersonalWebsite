ad613bab71a7266707678d013e5373c0dfe9b7c2
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2019-11-18 21:25:12 +0100

    Removed images from Projects page

--- app.js
+++ app.js
@@ -91,35 +91,19 @@ app.get('/projects', (req, res) =>
   var descriptions = [];
   var languages = [];
   var gitHubLinks = [];
-  var images1 = [];
-  var images2 = [];
-  var images3 = [];
-  var images4 = [];
-  var images5 = [];
 
   data.projects.forEach(function(item) {
     titles = titles.concat(item.title);
     descriptions = descriptions.concat(item.description);
     languages = languages.concat(item.language);
     gitHubLinks = gitHubLinks.concat(item.gitHub);
-    images1 = images1.concat(item.image1);
-    images2 = images2.concat(item.image2);
-    images3 = images3.concat(item.image3);
-    images4 = images4.concat(item.image4);
-    images5 = images5.concat(item.image5);
-
   });
 
     res.render('projects', {
       projectTitle: titles,
       projectDescription: descriptions,
       projectLanguage: languages,
-      projectGitHubLinks: gitHubLinks,
-      projectImages1: images1,
-      projectImages2: images2,
-      projectImages3: images3,
-      projectImages4: images4,
-      projectImages5: images5
+      projectGitHubLinks: gitHubLinks
     });
 });
 
@@ -138,17 +122,17 @@ transporter.sendMail(mailOptions, function(error){
   if (error) {
     console.log(error);
   } else {
-   
     res.redirect('/');
-    
   }
 });
 });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 /*
 app.listen(3000, function()
 {
         console.log('Listening on port 3000');
 }); */
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
